8703082241502f2ca674829239182285cd3a2358
lidiagarciapanadero <lidi_aura@hotmail.com>
2018-11-17 12:37:18 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z3#)cqsh@-^4sa1kt33&2+bq1)v&94tp+s!@=)nif2q461ad1i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ' . pythonanywhere.com','lidiaura.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', ' . pythonanywhere.com']
 
 
 # Application definition
